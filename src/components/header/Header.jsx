cfb73fe2abdd333dc2059d26eec753eea177b80d
naza <jnazarenaveron@gmail.com>
2024-01-12 19:34:51 -0300

    Mensaje descriptivo de los cambios

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import InstaIcon from './insta.svg'
+import InstaIcon from '../header/insta.svg'
 import './header.css';
 
 const Header = () => {
